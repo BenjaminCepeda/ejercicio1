bfc54367cc6f80efd0debcabac55a53ae3f34ca6
Benjamin Cepeda <bcepeda_98@yahoo.com>
2021-09-14 11:22:04 -0500

    implements currency search

--- converter/views.py
+++ converter/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
 from django.http import Http404
 from rest_framework import status
 from rest_framework.response import Response
@@ -11,5 +12,7 @@ from .models import Currencies
 
 
 class CurrenciesViewSet(viewsets.ModelViewSet):
+    filter_backends = (SearchFilter,)
     queryset = Currencies.objects.all().order_by('id')
     serializer_class = CurrenciesSerializer
+    search_fields = ('currency', 'query_date', )
